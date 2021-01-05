ecc38e22503a1ee6d947167dfe2137ac12aab964
gksthf2271 <rlagksthf33@naver.com>
2021-01-05 23:06:02 +0900

    장소명, 금액 EditText Watcher등록

--- app/src/main/java/com/khs/nbbang/freeUser/pageFragment/AddPlaceFragment.kt
+++ app/src/main/java/com/khs/nbbang/freeUser/pageFragment/AddPlaceFragment.kt
@@ -2,17 +2,22 @@ package com.khs.nbbang.freeUser.pageFragment
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.adapters.TextViewBindingAdapter
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.khs.nbbang.R
 import com.khs.nbbang.base.BaseFragment
 import com.khs.nbbang.databinding.FragmentAddPlaceBinding
+import com.khs.nbbang.freeUser.`interface`.TextWatcherAdapter
 import com.khs.nbbang.freeUser.viewModel.PageViewModel
 import kotlinx.android.synthetic.main.cview_add_edit_place.view.*
 import kotlinx.android.synthetic.main.cview_edit_place.view.*
@@ -57,12 +62,23 @@ class AddPlaceFragment : BaseFragment() {
             val infoView: ConstraintLayout =
                 inflater.inflate(R.layout.cview_edit_place, rootView, false) as ConstraintLayout
             infoView.btn_join.setOnClickListener {
-                showSelectPeopleDialog((rootView.indexOfChild(infoView) + 1).toString())
+                showSelectPeopleDialog(infoView.tag.toString())
             }
-            rootView.addView(infoView,rootView.childCount - 1)
-            var placeIndex = rootView.childCount - 1
-            infoView.txt_index.text = "$placeIndex 차"
-            mBinding.viewModel!!._placeCount.value = placeIndex
+
+            rootView.addView(infoView.apply {
+                var placeIndex = rootView.childCount - 1
+                infoView.tag = placeIndex
+                infoView.txt_index.text = "$placeIndex 차"
+
+                infoView.edit_title.addTextChangedListener(object : TextWatcherAdapter {
+                    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                        super.onTextChanged(s, start, before, count)
+                        mBinding.viewModel!!._selectedPeopleMap.value!!.get(infoView.tag as Int)
+                        //todo textWatcher <-> viewModel 상호작용 구현
+                    }
+                })
+                mBinding.viewModel!!._placeCount.value = placeIndex
+            },rootView.childCount - 1)
         }
         mBinding.viewModel.let {
             it!!._placeCount.observe(requireActivity(), Observer {
