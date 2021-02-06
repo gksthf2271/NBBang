df6ae968697dfa2b162229d74c5cf48e3adfe5b1
gksthf2271 <rlagksthf33@naver.com>
2021-02-06 17:52:01 +0900

    영수증 결과 복사 기능 추가

--- app/src/main/java/com/khs/nbbang/freeUser/pageFragment/ResultPageFragment.kt
+++ app/src/main/java/com/khs/nbbang/freeUser/pageFragment/ResultPageFragment.kt
@@ -1,14 +1,17 @@
 package com.khs.nbbang.freeUser.pageFragment
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.khs.nbbang.R
 import com.khs.nbbang.base.BaseFragment
-import com.khs.nbbang.databinding.FragmentAddPeopleBinding
 import com.khs.nbbang.databinding.FragmentResultPageBinding
 import com.khs.nbbang.freeUser.viewModel.PageViewModel
 
@@ -33,6 +36,18 @@ class ResultPageFragment : BaseFragment() {
                 requireActivity().application
             )
         ).get(PageViewModel::class.java)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mBinding.txtNotifyCopy.setOnClickListener {
+            val clipboard = getSystemService(requireContext(), ClipboardManager::class.java)
+            val clip: ClipData = ClipData.newPlainText(mBinding.txtTitle.text, mBinding.txtResult.text)
+            clipboard.let {
+                it!!.setPrimaryClip(clip)
+                Toast.makeText(requireContext(),"클립보드에 영수증이 복사되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onResume() {
