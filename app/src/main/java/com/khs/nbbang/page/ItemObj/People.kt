1be80a3a29519a0dc56a86674c54020cc06ba2dd
gksthf2271 <rlagksthf33@naver.com>
2021-02-14 22:43:40 +0900

    불필요 소스 삭제

--- app/src/main/java/com/khs/nbbang/page/ItemObj/People.kt
+++ app/src/main/java/com/khs/nbbang/page/ItemObj/People.kt
@@ -3,9 +3,6 @@ package com.khs.nbbang.page.ItemObj
 class People {
     var mName: String = ""
     var mIndex: Int = -1
-    var mNNBMoney: Int = 0
-    //Map<장소 아이디, 지불 가격>
-    var mJoinPlaceMap: MutableMap<Int, Int> = mutableMapOf()
 
     constructor(index:Int, name:String){
         mName = name
