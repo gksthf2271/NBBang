46747e0d0d9ddf7f93f78c3153637063d30df237
gksthf2271 <rlagksthf33@naver.com>
2021-05-11 23:43:05 +0900

    추가 불필요 소스 삭제

--- app/src/main/java/com/khs/nbbang/user/FreeUser.kt
+++
@@ -1,9 +0,0 @@
-package com.khs.nbbang.user
-
-import com.khs.nbbang.R
-
-data class FreeUser(
-    override var id: Long,
-    override var name: String = "FREE USER",
-    var imgRes: Int = R.drawable.icon_user
-) : User(id, name)
