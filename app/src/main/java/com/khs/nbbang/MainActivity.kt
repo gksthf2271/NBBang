deb2de8f6b125265e1fea28dd05baadaf5943c68
gksthf2271 <rlagksthf33@naver.com>
2022-02-17 23:55:54 +0900

    하단 메뉴로 변경

--- app/src/main/java/com/khs/nbbang/MainActivity.kt
+++ app/src/main/java/com/khs/nbbang/MainActivity.kt
@@ -1,33 +1,30 @@
 package com.khs.nbbang
 
-import android.graphics.Color
 import android.os.Bundle
-import android.util.TypedValue
 import android.view.KeyEvent
+import android.view.MenuItem
 import android.widget.Toast
-import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.NavHostFragment
-import com.khs.nbbang.animation.NavigationDrawerEvent
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.khs.nbbang.base.BaseActivity
 import com.khs.nbbang.base.BaseFragment
 import com.khs.nbbang.databinding.ActivityMainBinding
+import com.khs.nbbang.history.HistoryFragment
+import com.khs.nbbang.localMember.GroupManagementFragment
 import com.khs.nbbang.localMember.MemberManagementViewModel
 import com.khs.nbbang.login.LoginViewModel
+import com.khs.nbbang.mypage.MyPageFragment
+import com.khs.nbbang.page.DutchPayMainFragment
 import com.khs.nbbang.page.viewModel.PageViewModel
 import com.khs.nbbang.search.SearchLocalActivity
 import com.khs.nbbang.user.Member
-import com.khs.nbbang.utils.GlideUtils
 import com.khs.nbbang.utils.LogUtil
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
-import kotlinx.android.synthetic.main.cview_title_description.view.*
-import kotlinx.android.synthetic.main.nav_header_main.view.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     lateinit var mBinding: ActivityMainBinding
     private val mPageViewModel by viewModel<PageViewModel>()
     private val mLoginViewModel by viewModel<LoginViewModel>()
@@ -60,38 +57,17 @@ class MainActivity : BaseActivity() {
         mLoginViewModel.let { loginViewModel ->
             loginViewModel.checkKakaoLoginBySdk(this)
         }
-
-        mBinding.contentView.imgSearch.setOnClickListener {
-            launch<SearchLocalActivity>(startForResult)
-        }
     }
 
-    fun initNaviView() {
+    private fun initNaviView() {
         mNavHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        updateProfileInfo(null, null, null)
 
-        mBinding.drawerLayout.setScrimColor(Color.TRANSPARENT)
-        mBinding.drawerLayout.addDrawerListener(
-            NavigationDrawerEvent(
-                mBinding.layoutContent,
-                mBinding.navView
-            )
-        )
+        mBinding.navBottomView.setOnNavigationItemSelectedListener(this)
         addListenerAndObserver()
     }
 
     private fun addListenerAndObserver() {
-        group_indicator.setOnClickListener {
-            drawer_layout.openDrawer(GravityCompat.START)
-        }
-
-        mBinding.navView.setNavigationItemSelectedListener { menuItem ->
-            navigateMenu(menuItem.itemId)
-            true
-        }
-
-        mLoginViewModel
         mLoginViewModel.gMyData.observe(this, Observer { kakaoUser ->
             if (kakaoUser != null) {
                 LogUtil.vLog(LOG_TAG, TAG_CLASS, "mMyDataFrom : ${kakaoUser}")
@@ -106,7 +82,6 @@ class MainActivity : BaseActivity() {
                                 + "\n thumbnail_image : ${thumbnail}"
                                 + "\n connectedAt : ${kakaoUser.connectedAt}"
                     )
-                    updateProfileInfo(thumbnail, name, kakaoUser.kakaoAccount!!.email)
                     mMemberManagementViewModel.let { memberManagementViewModel ->
                         val myData = Member(
                             id = id,
@@ -117,90 +92,20 @@ class MainActivity : BaseActivity() {
                         )
                         memberManagementViewModel.saveKakaoMember(listOf(myData))
                     }
-                } else {
-                    updateProfileInfo(null, null, null)
                 }
 
             } else {
                 LogUtil.vLog(LOG_TAG, TAG_CLASS, "isLogin : $kakaoUser")
-                updateProfileInfo(null, null, null)
             }
         })
-
-        mBinding.navView.getHeaderView(0).setOnClickListener {
-            gotoMyPage()
-        }
-    }
-
-    private fun navigateMenu(menuId: Int) {
-        OLD_TAG = CURRENT_TAG
-        when (menuId) {
-            R.id.nav_dutch_pay -> {
-                mNavItemIndex = 0
-                CURRENT_TAG = TAG_DUTCH_PAY
-            }
-            R.id.nav_history -> {
-                mNavItemIndex = 1
-                CURRENT_TAG = TAG_HISTORY
-            }
-            R.id.nav_kakao_friends_settings -> {
-                mNavItemIndex = 2
-                CURRENT_TAG = TAG_KAKAO_FRIENDS_SETTINGS
-            }
-            R.id.nav_my_page -> {
-                mNavItemIndex = 3
-                CURRENT_TAG = TAG_MY_PAGE
-            }
-            else -> mNavItemIndex = 0
-        }
-        navigateDestination()
-    }
-
-    private fun updateProfileInfo(thumbnail: String?, name: String?, email: String?) {
-        val naviHeaderView = mBinding.navView.getHeaderView(0)
-        GlideUtils().drawImageWithString(naviHeaderView.img_profile, thumbnail, null)
-        naviHeaderView.group_name.txt_title.text = "이름"
-        naviHeaderView.group_id.txt_title.text = "계정"
-        naviHeaderView.group_name.txt_description.text = name ?: "FREE USER"
-        naviHeaderView.group_id.txt_description.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10.0f)
-        naviHeaderView.group_id.txt_description.text = email ?: "-"
     }
 
     private val gFinishToast by lazy {
         Toast.makeText(this, "한번 더 뒤로가기 입력 시 종료됩니다.", Toast.LENGTH_SHORT)
     }
 
-    override fun onBackPressed() {
-    }
-
     private fun gotoHome() {
-        LogUtil.vLog(LOG_TAG, TAG_CLASS, "loadHome(...)")
-        mNavItemIndex = 0
-        CURRENT_TAG = TAG_DUTCH_PAY
-        navigateDestination()
-    }
-
-    private fun gotoMyPage() {
-        mNavItemIndex = 3
-        CURRENT_TAG = TAG_MY_PAGE
-        navigateDestination()
-    }
-
-    private fun navigateDestination() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        }
-        navigation(CURRENT_TAG)
-    }
-
-    fun navigation(tag: String) {
-        when (tag) {
-            TAG_DUTCH_PAY -> mNavHostFragment.navController.navigate(R.id.action_go_to_dutch_pay)
-            TAG_HISTORY -> mNavHostFragment.navController.navigate(R.id.action_go_to_history)
-            TAG_KAKAO_FRIENDS_SETTINGS -> mNavHostFragment.navController.navigate(R.id.action_go_to_kakao_friends_settings)
-            TAG_MY_PAGE -> mNavHostFragment.navController.navigate(R.id.action_go_to_my_page)
-        }
-        mBinding.navView.menu.getItem(mNavItemIndex).isChecked = true
+        mBinding.navBottomView.selectedItemId = R.id.nav_dutch_pay
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
@@ -214,9 +119,7 @@ class MainActivity : BaseActivity() {
         else {
             when (keyCode) {
                 KeyEvent.KEYCODE_BACK -> {
-                    if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-                        drawer_layout.closeDrawer(GravityCompat.START)
-                    } else if (CURRENT_TAG == TAG_DUTCH_PAY) {
+                    if (CURRENT_TAG == TAG_DUTCH_PAY) {
                         if (gFinishToast != null) {
                             setResult(RESULT_FINISH)
                             finish()
@@ -231,5 +134,32 @@ class MainActivity : BaseActivity() {
             }
         }
         return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_dutch_pay -> {
+                supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment , DutchPayMainFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.nav_history -> {
+                supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, HistoryFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.nav_search -> {
+                launch<SearchLocalActivity>(startForResult)
+                return true
+            }
+            R.id.nav_kakao_friends_settings -> {
+                supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, GroupManagementFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.nav_my_page -> {
+                supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, MyPageFragment()).commitAllowingStateLoss()
+                return true
+            }
+        }
+
+        return false
     }
 }
